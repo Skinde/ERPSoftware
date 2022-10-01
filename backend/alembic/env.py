80cd3db83c135e32d491b2416c795e1b52442dd6
Marcos-1001 <marcos.ayala@utec.edu.pe>
2022-10-01 15:55:36 -0500

    Alembic fuera. Cada uno con sus migraciones

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -21,7 +21,6 @@ if config.config_file_name is not None:
 from models import _database
 target_metadata = _database.Base.metadata
 
-
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
