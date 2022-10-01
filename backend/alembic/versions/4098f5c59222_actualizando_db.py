07aba07455a6366431de7e1cbd2ed27165ba38bc
Marcos-1001 <marcos.ayala@utec.edu.pe>
2022-10-01 14:49:35 -0500

    cambios

---
+++ backend/alembic/versions/4098f5c59222_actualizando_db.py
@@ -0,0 +1,24 @@
+"""actualizando db
+
+Revision ID: 4098f5c59222
+Revises: 1cb46d31da85
+Create Date: 2022-10-01 14:03:45.551054
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4098f5c59222'
+down_revision = '1cb46d31da85'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
---
+++ backend/alembic/versions/921da27785ba_actualizando_db.py
@@ -0,0 +1,24 @@
+"""actualizando db
+
+Revision ID: 921da27785ba
+Revises: 9d77c2debf24
+Create Date: 2022-10-01 13:59:11.509316
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '921da27785ba'
+down_revision = '9d77c2debf24'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
