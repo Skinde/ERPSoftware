302f56f42161228e666e00ab8f28675baffe3f5c
Marcos-1001 <marcos.ayala@utec.edu.pe>
2022-10-16 18:53:16 -0500

    conexion de front con back

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './styles/App_Login.css';
+//import LoginForm from "./js/LoginForm.js";
+import Login from "./components/Login";
+
+
+const App = () => {
+    
+    
+    return (
+        <div className="App">
+            
+            <Login />
+            
+        </div>
+    )
+}
+
+export default App;
