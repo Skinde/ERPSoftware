aa7e2cba3612472e40c220ebb382b1ee20c6e5d6
JuanTorres7 <juan.torres.f@utec.edu.pe>
2022-10-19 18:20:57 -0500

    Data table

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -2,11 +2,61 @@ import React, { useContext } from "react";
 import './../styles/Main_page.css';
 import { useNavigate } from "react-router-dom";
 import { UserContext, _cookies } from "../context/UserContext";
-
+import DataTable from "react-data-table-component";
 
 
 const l_icon_style = {marginTop: '6px'}
 const l_icon = require('./../oficial_icon.png');
+
+const randomTable = [
+    {Tittle:"Introduction to Algorithms", Author:"Thomas H., Cormen", Edition:"4th", 
+        Publisher:"The MIT press", Genre: "Education", Year: "2021", ISBN: "978-0262046305"},
+    {Tittle:"Computability, Complexity, and Languages: Fundamentals of Theoretical Computer Science", Author:"Martin D., Davis", Edition:"2nd",
+        Publisher:"Morgan Kaufmann", Genre: "Education", Year: "1994", ISBN: "978-0122063824"},
+    {Tittle:"The Universal Computer: The Road from Leibniz to Turing", Author:"Davis, Martin", Edition:"1st",
+        Publisher:"W. W. Norton & Company", Genre: "Education", Year: "2000", ISBN: "978-0393047851"},
+    {Tittle:"Operating systems: Design and implementation", Author:"Tanenbaum A., Woodhull, A.", Edition:"3rd",
+        Publisher:"Pearson", Genre: "Education", Year: "2006", ISBN: "978-0131429383"},
+
+    {Tittle:"Operating System Concepts", Author:"Silberchatz, Abraham", Edition:"10nd",
+        Publisher:"Wiley", Genre: "Education", Year: "2021", ISBN: "978-1119800361"},
+    {Tittle:"Operating System Concepts", Author:"Silberchatz, Abraham", Edition:"10nd",
+        Publisher:"Wiley", Genre: "Education", Year: "2021", ISBN: "978-1119800361"},
+    {Tittle:"Operating System Concepts", Author:"Silberchatz, Abraham", Edition:"10nd",
+        Publisher:"Wiley", Genre: "Education", Year: "2021", ISBN: "978-1119800361"},
+    {Tittle:"Operating System Concepts", Author:"Silberchatz, Abraham", Edition:"10nd",
+        Publisher:"Wiley", Genre: "Education", Year: "2021", ISBN: "978-1119800361"},
+    {Tittle:"Operating System Concepts", Author:"Silberchatz, Abraham", Edition:"10nd",
+        Publisher:"Wiley", Genre: "Education", Year: "2021", ISBN: "978-1119800361"},
+    {Tittle:"Operating System Concepts", Author:"Silberchatz, Abraham", Edition:"10nd",
+        Publisher:"Wiley", Genre: "Education", Year: "2021", ISBN: "978-1119800361"},
+    {Tittle:"Operating System Concepts", Author:"Silberchatz, Abraham", Edition:"10nd",
+        Publisher:"Wiley", Genre: "Education", Year: "2021", ISBN: "978-1119800361"},
+];
+
+const columns = [
+    { name: "Tittle", selector: "Tittle", sortable: true, center: false, left: true, grow: 1.5 },
+    { name: "Author", selector: "Author", sortable: true, center: false, right: true },
+    { name: "Edition", selector: "Edition", sortable: true, center: false, right: true, grow: 0.5 },
+    { name: "Publisher", selector: "Publisher", sortable: true, center: false, right: true },
+    { name: "Genre", selector: "Genre", sortable: true, center: false, right: true },
+    { name: "Year", selector: "Year", sortable: true, center: false, right: true, grow: 0.5 },
+    { name: "ISBN", selector: "ISBN", sortable: false, center: false, right: true }
+]
+
+const customStyles = {
+    rows: {
+        style: { minHeight: '40px', paddingRight: '10px' },
+    },
+    cells: {
+        style: {
+            paddingLeft: '8px',
+            paddingRight: '8px',
+        },
+    },
+}
+
+const paginationOptions = { rowsPerPageText: '' }
 
 const MainPage = () => {
     const [token] = useContext(UserContext);
@@ -64,6 +114,7 @@ const MainPage = () => {
                         <option value="" selected disabled>Genre</option>
                         <option value="value_1">Action</option>
                         <option value="value_2">Adventure</option>
+                        <option value="value_3">Education</option>
                     </select>
                 </div>
 
@@ -76,7 +127,17 @@ const MainPage = () => {
             </form>
 
             <div class="Results-Wrapper">
-
+                <div class="For-table">
+                    <DataTable
+                        columns={columns}
+                        data={randomTable}
+                        customStyles = {customStyles}
+                        pagination
+                        paginationComponentOptions={paginationOptions}
+                        paginationRowsPerPageOptions={[]}
+                        fixedHeader
+                    />
+                </div>
             </div>
         </div>
     )
